595c946be7b5b11fc11795865ac0cd44c8aa5565
lawonder <3259691336@qq.com>
2017-04-30 23:21:17 +0800

    try

---
+++ xyw/src/rj7/dao/tuisong/tryandtry.java
@@ -0,0 +1,20 @@
+package rj7.dao.tuisong;
+
+public class tryandtry {
+	/*Tuisong ts = new Tuisong();
+	ts.setAdmid("8");
+	ts.setDianzancnt("0");
+	ts.setBrowsecnt("0");;
+	ts.setPingluncnt("0");
+	ts.setContent("hahaha");
+	ts.setHead("yoyoyo");
+	ts.setTid(null);			//设置为null后，从上一个id开始+1
+	ts.setStatus("1");
+	ts.setType("公告");
+	ts.setContent("aiyouwei");
+	
+    TuisongDAOProxy t = new TuisongDAOProxy();
+    if(t.doCreate(ts)){
+    	System.out.println("====================addok");
+    }*/
+}
