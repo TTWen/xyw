94bd0ca7f7c134e1d135d924c69fa24576484109
shiguomei <2097424498@qq.com>
2017-05-02 13:15:07 +0800

    增删改，测试通过

---
+++ xyw/src/rj7/dao/real/RealDAOProxy.java
@@ -0,0 +1,42 @@
+package rj7.dao.real;
+
+
+import rj7.bean.Real;
+
+
+public class RealDAOProxy implements IRealDAO {
+	
+
+private IRealDAO dao = null;
+	
+	public RealDAOProxy() {
+		
+		this.dao = new RealDAOImpl();
+	}
+	
+    //通过ID获得数据	
+	public Real findById(String id, String tblname,String idtype) {
+		
+		return this.dao.findById(id, tblname,idtype);
+	}
+
+	@Override
+	public int insertReal(Real real) {
+		// TODO Auto-generated method stub
+		return this.dao.insertReal(real);
+	}
+
+	@Override
+	public int deleteReal(String id) {
+		// TODO Auto-generated method stub
+		return this.dao.deleteReal(id);
+	}
+
+	@Override
+	public int editReal(Real real) {
+		// TODO Auto-generated method stub
+		return this.dao.editReal(real);
+	}
+	
+}
+//
---
+++ xyw/src/rj7/dao/real/Test.java
@@ -0,0 +1,52 @@
+package rj7.dao.real;
+
+
+import rj7.bean.Real;
+import rj7.util.DAOFactory;
+
+
+/**
+ * @author 史国梅
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		RealDAOProxy proxy= new RealDAOProxy();
+		String id ="2";
+		String idtype="rid";
+		Real real = (Real)DAOFactory.getDaoInstance().findById("tblreal", id, idtype, Real.class);
+		
+		System.out.println(real.getAdress());
+		System.out.println(real.getFilesurl());
+		System.out.println(real.getIdcard());
+		System.out.println(real.getRid());
+		
+		//修改表
+		real.setAdress("shiguomei");
+		proxy.editReal(real);
+		Real real2 = (Real)DAOFactory.getDaoInstance().findById("tblreal", id, idtype, Real.class);
+		System.out.println(real.getAdress());
+		System.out.println(real2.getAdress());
+		System.out.println(real2.getFilesurl());
+		System.out.println(real2.getIdcard());
+		System.out.println(real2.getRid());
+		
+		
+		//删除表
+		proxy.deleteReal("3");
+		//插入表
+				Real real1 = new Real();
+				real1.setRid("4");
+				real1.setIdcard("1234567890");
+				proxy.insertReal(real1);
+				System.out.println(real1.getAdress());
+				System.out.println(real1.getFilesurl());
+				System.out.println(real1.getIdcard());
+				System.out.println(real1.getRid());
+				
+		
+		
+		
+		
+	}
+}
